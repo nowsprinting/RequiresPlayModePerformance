734620a4d4aba708396ab5d5219dc633b35f889f
Koji Hasegawa <hasegawa@hubsys.co.jp>
2022-02-03 08:45:18 +0900

    Add tests

---
+++ Assets/Editor/RunTestsPerformance.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) 2022 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using System;
+using UnityEditor;
+using UnityEditor.TestTools.TestRunner.Api;
+using UnityEngine;
+
+public class RunTestsPerformance : ICallbacks
+{
+    private static int s_enterPlayModeCount;
+    private DateTime _runStartedTime;
+
+    [InitializeOnLoadMethod]
+    private static void SetupCallbacks()
+    {
+        var api = ScriptableObject.CreateInstance<TestRunnerApi>();
+        api.RegisterCallbacks(new RunTestsPerformance());
+    }
+
+    [InitializeOnEnterPlayMode]
+    private static void OnEnterPlayMode()
+    {
+        s_enterPlayModeCount++;
+        Debug.Log(nameof(OnEnterPlayMode)); // RunStartedより前に呼ばれるのでカウントは無効。ログで判断
+    }
+
+    public void RunStarted(ITestAdaptor testsToRun)
+    {
+        s_enterPlayModeCount = 0;
+        _runStartedTime = DateTime.Now;
+        Debug.Log(nameof(RunStarted));
+    }
+
+    public void RunFinished(ITestResultAdaptor result)
+    {
+        var time = DateTime.Now.Subtract(_runStartedTime);
+        Debug.Log($"RunFinished. Time={time.TotalMilliseconds}[ms], OnEnterPlayMode={s_enterPlayModeCount}");
+    }
+
+    public void TestStarted(ITestAdaptor test)
+    {
+        // nop
+    }
+
+    public void TestFinished(ITestResultAdaptor result)
+    {
+        // nop
+    }
+}
---
+++ Assets/RequiresPlayModePerformance2/Tests/Runtime/PlayModeTests.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) 2022 Koji Hasegawa.
+// This software is released under the MIT License.
+
+using NUnit.Framework;
+
+namespace RequiresPlayModePerformance.Tests.Runtime
+{
+    public class PlayModeTests
+    {
+        [Test]
+        public void DummyPlayModeTest_AlwaysPassed([Range(1, 100)] int i)
+        {
+            Assert.That(true, Is.True);
+        }
+    }
+}
