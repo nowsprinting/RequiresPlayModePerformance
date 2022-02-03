56ad1a6a787890c63bfcc76de979df1e9ab15d49
Koji Hasegawa <hasegawa@hubsys.co.jp>
2022-02-03 10:21:03 +0900

    Split Edit/ Play Mode tests by RequiresPlayModeAttribute

--- Assets/RequiresPlayModePerformance/Tests/Editor/EditModeTests.cs
+++ Assets/RequiresPlayModePerformance/Tests/Editor/EditModeTests.cs
@@ -2,13 +2,21 @@
 // This software is released under the MIT License.
 
 using NUnit.Framework;
+using UnityEngine.TestTools;
 
 namespace RequiresPlayModePerformance.Tests.Editor
 {
     public class EditModeTests
     {
         [Test]
-        public void DummyEditModeTest_AlwaysPassed([Range(1, 100)] int i)
+        public void DummyEditModeTest_AlwaysPassed([Range(1, 50)] int i)
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Test]
+        [RequiresPlayMode]
+        public void DummyPlayModeTestInEditor_AlwaysPassed([Range(51, 100)] int i)
         {
             Assert.That(true, Is.True);
         }
--- Assets/RequiresPlayModePerformance5/Tests/Runtime/PlayModeTests.cs
+++ Assets/RequiresPlayModePerformance5/Tests/Runtime/PlayModeTests.cs
@@ -2,13 +2,21 @@
 // This software is released under the MIT License.
 
 using NUnit.Framework;
+using UnityEngine.TestTools;
 
 namespace RequiresPlayModePerformance.Tests.Runtime
 {
     public class PlayModeTests
     {
         [Test]
-        public void DummyPlayModeTest_AlwaysPassed([Range(1, 100)] int i)
+        public void DummyPlayModeTest_AlwaysPassed([Range(1, 50)] int i)
+        {
+            Assert.That(true, Is.True);
+        }
+
+        [Test]
+        [RequiresPlayMode(false)]
+        public void DummyEditModeTestInPlayer_AlwaysPassed([Range(51, 100)] int i)
         {
             Assert.That(true, Is.True);
         }
